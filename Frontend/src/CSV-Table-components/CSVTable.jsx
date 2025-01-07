d064d32f732a6998856c6a269dd1c5fece2a3a12
vedansh-tyagi245 <vedanshtyagi245@gmail.com>
2025-01-07 16:56:24 +0530

    more designed table

--- Frontend/src/CSV-Table-components/CSVTable.jsx
+++ Frontend/src/CSV-Table-components/CSVTable.jsx
@@ -5,14 +5,14 @@ function CSVTable({ csvData, setCsvData }) {
         <div>
             {/* Display CSV Data in Table */}
             {csvData && (
-                <div className="overflow-y-auto max-h-[80vh] p-0 border border-gray-300 rounded-md" style={{ boxShadow: '0 2px 10px rgba(0,0,0,0.1)' }}>
-                    <table className="table-auto w-full border-collapse border border-gray-200">
+                <div className="overflow-y-auto overflow-x-auto max-w-[90vw] max-h-[80vh] p-0 border rounded-md text-center mx-auto" style={{ boxShadow: '0 2px 10px rgba(0,0,0,0.1)' }}>
+                    <table className="table-auto w-full">
                         <thead>
                             <tr>
                                 {Object.keys(csvData[0]).map((header, index) => (
                                     <th
                                         key={index}
-                                        className="border p-2 text-left bg-gray-100 sticky top-0 z-10"
+                                        className="border border-gray-500 p-2 bg-black bg-opacity-60 text-white sticky top-0 z-10 text-center"
                                     >
                                         {header}
                                     </th>
@@ -23,7 +23,7 @@ function CSVTable({ csvData, setCsvData }) {
                             {csvData.map((row, index) => (
                                 <tr key={index} className="border-b">
                                     {Object.values(row).map((value, index) => (
-                                        <td key={index} className="border p-2">
+                                        <td key={index} className="border border-gray-500 p-2 text-white bg-black bg-opacity-50 hover:bg-opacity-5">
                                             {value}
                                         </td>
                                     ))}
@@ -38,4 +38,3 @@ function CSVTable({ csvData, setCsvData }) {
 }
 
 export default CSVTable;
-    
